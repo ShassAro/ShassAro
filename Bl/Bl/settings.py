118ff52fb5565711f2ea77973e08037db446a12c
scarlatshay <scarlat.shay@gmail.com>
2015-03-18 23:45:54 +0200

    Changed database location to relative

--- Bl/Bl/settings.py
+++ Bl/Bl/settings.py
@@ -60,8 +60,7 @@ WSGI_APPLICATION = 'Bl.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME': '/home/assaf/PycharmProjects/ShassAro/Bl/db.sqlite3'
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
