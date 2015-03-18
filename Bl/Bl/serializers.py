8ed7657e000da986c7785676d863ffb41336f3ce
scarlatshay <scarlat.shay@gmail.com>
2015-03-19 01:24:22 +0200

    Added Serializers and ViewSets to all Models. Basic Access tests performed.

--- Bl/Bl/serializers.py
+++ Bl/Bl/serializers.py
@@ -9,3 +9,60 @@ class TagSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Tag
         fields = ('pk', 'name', 'description')
+
+
+class ImageSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Image
+        fields = ('pk', 'docker_name', 'description', 'tags', 'level', 'allow_in_game', 'hints',
+                  'goal_description', 'post_script_name', 'duration_minutes')
+
+
+class LearningPathSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = LearningPath
+        fields = ('pk', 'subject', 'images', 'color', 'description')
+
+
+class GameUserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = GameUser
+        fields = ('pk', 'name', 'password', 'vnc_port')
+
+
+class ShassaroSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Shassaro
+        fields = ('pk', 'goals', 'participants', 'shassaro_ip', 'docker_server_ip', 'docker_id')
+
+
+class GameSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Game
+        fields = ('pk', 'group1', 'group2', 'computer', 'images', 'shassaros', 'start_time', 'goals_completed',
+                  'duration_minutes')
+
+
+class GameResultSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = GameResult
+        fields = ('pk', 'losing_users', 'winning_users', 'computer', 'start_time', 'actual_duration_minutes', 'tags',
+                  'experience_gained')
+
+
+class BadgeSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Badge
+        fields = ('pk', 'name', 'class_name', 'experience')
+
+
+class DockerServerSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = DockerServer
+        fields = ('pk', 'name', 'ip')
+
+
+class ConfigurationsSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Configurations
+        fields = ('pk', 'docker_server')
