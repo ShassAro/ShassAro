534d362423ac84db8da00738e38b22a17275f288
scarlatshay <scarlat.shay@gmail.com>
2015-03-24 17:13:46 +0200

    Added a few 'unique' fields where I thought they were necessary. Removed the 'primary_key=True' from a few classes, as they were causing issues (duplicated values)...

---
+++ Bl_project/Bl/models.py
@@ -0,0 +1,81 @@
+from django.contrib.auth.models import User
+from jsonfield import JSONField
+from django.db import models
+
+__author__ = 'shay'
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+
+
+class Image(models.Model):
+    docker_name = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+    tags = models.ManyToManyField(Tag)
+    level = models.PositiveIntegerField()
+    allow_in_game = models.BooleanField(default=False)
+    hints = JSONField() # represented as a json array
+    goal_description = JSONField() # JSON describing the goals in this image
+    post_script_name = models.TextField() # String to represent either script name, or script path
+    duration_minutes = models.PositiveIntegerField()
+
+
+class LearningPath(models.Model):
+    subject = models.CharField(max_length=100, unique=True)
+    images = models.ManyToManyField(Image)
+    # picture = models.ImageField()
+    color = models.CharField(max_length=100)
+    description = models.TextField()
+
+
+class GameUser(models.Model):
+    name = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    vnc_port = models.PositiveIntegerField()
+
+
+class Shassaro(models.Model):
+    goals = JSONField() #JSON-serialized (text) version of the goals
+    participants = models.ManyToManyField(GameUser)
+    shassaro_ip = models.IPAddressField()
+    docker_server_ip = models.IPAddressField()
+    docker_id = models.CharField(max_length=100)
+
+
+class Game(models.Model):
+    group1 = models.ManyToManyField(User, related_name='group1')
+    group2 = models.ManyToManyField(User, related_name='group2')
+    computer = models.BooleanField(default=False)
+    images = models.ManyToManyField(Image)
+    shassaros = models.ManyToManyField(Shassaro)
+    start_time = models.DateTimeField()
+    goals_completed = JSONField()
+    duration_minutes = models.PositiveIntegerField()
+
+
+class GameResult(models.Model):
+    losing_users = models.ManyToManyField(User, related_name='losing_users')
+    winning_users = models.ManyToManyField(User, related_name='winning_users')
+    computer = models.BooleanField(default=False)
+    start_time = models.DateTimeField()
+    actual_duration_minutes = models.PositiveIntegerField()
+    tags = models.ManyToManyField(Tag)
+    experience_gained = models.PositiveIntegerField()
+
+
+class Badge(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    #icon = models.ImageField()
+    class_name = models.CharField(max_length=100)
+    experience = models.PositiveIntegerField()
+
+
+class DockerServer(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    ip = models.IPAddressField(unique=True)
+
+
+class Configurations(models.Model):
+    docker_server = models.ManyToManyField(DockerServer)
