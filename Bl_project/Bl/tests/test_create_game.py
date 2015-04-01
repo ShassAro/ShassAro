3ac227bdd9350227afac2a6cda63331351f6b49b
scarlatshay <scarlat.shay@gmail.com>
2015-04-01 21:07:59 +0300

    removed __init__.py file from Bl_project directory

--- Bl_project/Bl/tests/test_create_game.py
+++ Bl_project/Bl/tests/test_create_game.py
@@ -1,3 +1,5 @@
+"""
+
 from unittest import TestCase
 from Bl_project.Bl.create_game import deploy_shassaros
 from Bl_project.Bl.exceptions import *
@@ -9,3 +11,5 @@ class when_calling_deploy_shassaros_with_invalid_data(TestCase):
     def it_should_raise_ValueError(self):
         with self.assertRaises(ValueError):
             deploy_shassaros(None)
+
+"""
