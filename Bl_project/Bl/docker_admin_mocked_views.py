2be93e4a4d2dd319666ca3f168b92e2a551ad5fa
assaf <assafaloni@gmail.com>
2015-05-02 12:24:22 +0300

    added authentication to the unit tests

--- Bl_project/Bl/docker_admin_mocked_views.py
+++ Bl_project/Bl/docker_admin_mocked_views.py
@@ -1,21 +1,23 @@
-from Bl.serializers import ShassaroSerializer
-from django.views.decorators.csrf import csrf_exempt
+from serializers import ShassaroSerializer
+from django.core import serializers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.request import Request
 from rest_framework.status import *
-from rest_framework.views import APIView
 
 
-@api_view(['GET'])
+@api_view(['POST'])
 def deploy(request):
-    if request.method == 'GET':
-        if request.data is None:
-            return Response("Empty input", status=HTTP_400_BAD_REQUEST)
-        serializer = ShassaroSerializer(data=request.data, many=True)
-        if not serializer.is_valid():
-            return Response("Invalid input",status=HTTP_400_BAD_REQUEST)
+    if request.method == 'POST':
+        shassaros = serializers.deserialize("json", request.data)
 
-        shassaros = serializer.data
+        # serializer = ShassaroSerializer(data=request.data["shassaros"], many=True, context={'request': request})
+        # if not serializer.is_valid():
+        #     return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+        #
+        # shassaros = serializer.data
+        if len(shassaros) != 2:
+            return Response("Must provide exactly 2 shassaro objects", status=HTTP_400_BAD_REQUEST)
         for shassaro in shassaros:
             shassaro.shassaro_ip = "192.168.1.13"
             shassaro.docker_server_ip = "192.168.2.1"
