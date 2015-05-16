c520f1982bb026d76eebbe249cf48e8e8e8c5069
roiravhon <curioso.roi@gmail.com>
2015-05-16 18:43:04 +0300

    Updated init_test_data

--- Bl_project/Bl/init_test_data.py
+++ Bl_project/Bl/init_test_data.py
@@ -16,8 +16,12 @@ def init_data():
     status = GameRequestStatus(status="ERROR", message="Sorry... no challenges for you today :(")
     status.save()
 
-    img = Image(docker_name="shassaro/challenge1", description="POC image", level=1, allow_in_game=True, hints=["Try to tahat (not too much tahat)"], goal_description=['tahat','another-tahat'], duration_minutes=60)
+    img = Image(docker_name="shassaro/challenge1", description="POC image", level=1, allow_in_game=True, hints=["Try to tahat (not too much tahat)", "Keep that in mind. Tahat is the shit."], goal_description=['tahat','another-tahat'], duration_minutes=60)
     img.save()
+
+    img = Image(docker_name="shassaro/challenge2", description="Reverse shell", level=1, allow_in_game=True, hints=["The bash version have not been updated in a while..", "Look at the website source.."], goal_description=['Find reverse shell!','Get the secret from the DB'], duration_minutes=60)
+    img.save()
+
 
     docker_server = DockerServer(name="primary", protocol="http", ip="127.0.0.1", port="4243")
     docker_server.save()
