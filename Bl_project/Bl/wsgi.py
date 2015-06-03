ca364a85ce403540a0faa51ee5dec4ef9e7f1753
roiravhon <curioso.roi@gmail.com>
2015-06-03 16:15:16 +0300

    Updated wsgi to support apache basic auth

--- Bl_project/Bl/wsgi.py
+++ Bl_project/Bl/wsgi.py
@@ -10,6 +10,8 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 import os
 import sys
 
+from django.contrib.auth.handlers.modwsgi import check_password
+
 root = os.path.join(os.path.dirname(__file__), '..')
 sys.path.insert(0,root)
 #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Bl.settings")
