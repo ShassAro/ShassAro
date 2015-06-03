ed436f6219e1ba01e2c18eea411fb51f1feddd3e
roiravhon <curioso.roi@gmail.com>
2015-06-03 18:54:56 +0300

    updated websockets wsgi

--- Bl_project/Bl/websocket-wsgi.py
+++ Bl_project/Bl/websocket-wsgi.py
@@ -2,6 +2,6 @@ import os
 import gevent.socket
 import redis.connection
 redis.connection.socket = gevent.socket
-os.environ.update(DJANGO_SETTINGS_MODULE='Bl.settings')
+os.environ["DJANGO_SETTINGS_MODULE"] = "Bl.settings"
 from ws4redis.uwsgi_runserver import uWSGIWebsocketServer
 application = uWSGIWebsocketServer()
