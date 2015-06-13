738e53c393f01d6277e3419442a0c3e3e1d89b67
Assaf Aloni <assafaloni@gmail.com>
2015-06-13 13:06:17 +0300

    User session is now working

--- Web-UI/app/constants/AUTH_EVENTS.js
+++ Web-UI/app/constants/AUTH_EVENTS.js
@@ -8,3 +8,10 @@ ShassaroApp.constant('AUTH_EVENTS',{
     notAuthenticated: 'auth-not-authenticated',
     notAuthorized: 'auth-not-authorized'
 });
+
+ShassaroApp.constant('SETTINGS',{
+    //apiUrl: 'http://10.0.0.9:1234',
+    apiUrl: 'http://www.shassaro.com/api/bl',
+    //wsUrl: 'ws://10.0.0.9:1234/ws/'
+    wsUrl: 'ws://www.shassaro.com/ws/'
+});
