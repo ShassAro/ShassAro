916ee5da401da2a234099c021ac05545a41116c2
shay <scarlat.shay@gmail.com>
2015-06-13 23:20:08 +0300

    Changed scavager URL #3

--- Bl_project/Bl/scripts/scavager_for_cron.py
+++ Bl_project/Bl/scripts/scavager_for_cron.py
@@ -15,7 +15,7 @@ def main():
 
         urllib2.install_opener(opener)
         try:
-            request = urllib2.Request('http://localhost:1234/scavage/', '')
+            request = urllib2.Request(SERVER_URL + '/scavage/', '')
             response = urllib2.urlopen(request)
         except urllib2.HTTPError as e:
             print str(datetime.now()) + ' - Server could not complete request. Err: ' + str(e.code)
